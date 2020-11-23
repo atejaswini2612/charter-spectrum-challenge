437d1544ed277a7385fa36256f23a96a4a3b6b6a
atejaswini2612 <atejaswini2612@gmail.com>
2020-11-23 01:33:40 -0800

    Add app header component

---
+++ Documents/charter-spectrum-challenge/src/components/header/AppHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppHeader.css";
+
+const AppHeader = () => {
+  return (
+    <header className="app-header">
+      <h1 className="app-title">Restaurants App</h1>
+    </header>
+  );
+};
+
+export default AppHeader;
